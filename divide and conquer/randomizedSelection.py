871de90a8eec5a70607bde086a4aa953efab2d4c
vishnu teja paturu <vishnu.paturu@mindtree.com>
2020-06-10 15:23:34 +0530

    randomized selection algorithm

--- divide and conquer/randomizedSelection.py
+++ divide and conquer/randomizedSelection.py
@@ -5,24 +5,28 @@ Created on Wed Jun 10 08:38:50 2020
 @author: Vishnu
 """
 
+
 def swapValues(arr, i, j):
     t = arr[i]
     arr[i] = arr[j]
     arr[j] = t
 
+
 def chooseRandomPivot(array, startIndex, endIndex):
     return endIndex
+
 
 def arrayPartition(array, startIndex, endIndex):
     randomPivot = chooseRandomPivot(array, startIndex, endIndex)
     swapValues(array, randomPivot, endIndex)
     pivot = startIndex
     for i in range(startIndex, endIndex):
-        if  array[i] < array[endIndex]:
+        if array[i] < array[endIndex]:
             swapValues(array, i, pivot)
-            pivot +=1
+            pivot += 1
     swapValues(array, pivot, endIndex)
     return pivot
+
 
 def randomizedSelection(array, startIndex, endIndex):
     global ithOrderIndex
@@ -33,10 +37,9 @@ def randomizedSelection(array, startIndex, endIndex):
         return randomizedSelection(array, startIndex, pivot - 1)
     else:
         return randomizedSelection(array, pivot + 1, endIndex)
-    
-    
 
-array = [8, 4, 33, 2, 55, 1 , 77, 21, 65, 10 ]
+
+array = [8, 4, 33, 2, 55, 1, 77, 21, 65, 10]
 
 
 ithOrderIndex = 2
@@ -44,7 +47,3 @@ ithOrderIndex = 2
 for i in range(10):
     ithOrderIndex = i
     print(randomizedSelection(array, 0, len(array)-1))
-
-    
-    
-    
